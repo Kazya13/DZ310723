2dc03646554dcd08cf45550ade49b1354f8a064a
Kazya13 <tatyana.a.kim@gmail.com>
2023-08-14 20:08:03 +1100

    tasks 25, 27, 29

---
+++ Task027/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)//исправление конвертации строки в int
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine()!;
+    int result = Convert.ToInt32(readInput);
+    return result;
+}
+
+int DigitSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10; //result = result + (number % 10);
+        number /=10; // number = number / 10;
+    }
+    return result;
+}
+
+int num = Prompt("Input number: ");
+Console.WriteLine();
+
+Console.WriteLine($"Number={num} DigitSum = {DigitSum(num)}");
